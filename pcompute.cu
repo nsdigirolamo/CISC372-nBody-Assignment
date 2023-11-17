1cb35518f5d5d74a307453c65a59aa43d5740154
Nicholas DiGirolamo <nickdigi@ubuntu-2004.eecis.udel.edu>
2023-11-17 15:40:54 -0500

    Reset to state where output matches serialized version.

--- pcompute.cu
+++ pcompute.cu
@@ -60,6 +60,7 @@ void compute () {
 		accels[i] = &values[i * NUMENTITIES];
 	}
 
+	/**
 	dim3 blocks(NUMENTITIES, BLOCKS_PER_ROW);
 	dim3 threads(THREADS_PER_BLOCK);
 
@@ -70,20 +71,35 @@ void compute () {
 		printf("Kernel Launch Failed with Error: %s\n", cudaGetErrorString(err));
 	
 	cudaDeviceSynchronize();
+	*/
 
-	for (i = 0; i < NUMENTITIES; i++) {
-
-		vector3 accel_sum={0,0,0};
-
-		for (j = 0; j < NUMENTITIES; j++) {
-			for (k = 0; k < 3; k++) {
-				accel_sum[k] += accels[i][j][k];
+	for (i=0;i<NUMENTITIES;i++){
+		for (j=0;j<NUMENTITIES;j++){
+			if (i==j) {
+				FILL_VECTOR(accels[i][j],0,0,0);
+			}
+			else{
+				vector3 distance;
+				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
+				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+				double magnitude=sqrt(magnitude_sq);
+				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
+				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
 			}
 		}
-
-		for (k = 0; k < 3; k++){
-			hVel[i][k] += accel_sum[k] * INTERVAL;
-			hPos[i][k] += hVel[i][k] * INTERVAL;
+	}
+	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+	for (i=0;i<NUMENTITIES;i++){
+		vector3 accel_sum={0,0,0};
+		for (j=0;j<NUMENTITIES;j++){
+			for (k=0;k<3;k++)
+				accel_sum[k]+=accels[i][j][k];
+		}
+		//compute the new velocity based on the acceleration and time interval
+		//compute the new position based on the velocity and time interval
+		for (k=0;k<3;k++){
+			hVel[i][k]+=accel_sum[k]*INTERVAL;
+			hPos[i][k]+=hVel[i][k]*INTERVAL;
 		}
 	}
 
