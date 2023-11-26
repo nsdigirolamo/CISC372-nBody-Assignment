f3458ff69ebee07026fc3089bf42053b77ff2cb8
Nicholas DiGirolamo <nickdigi@ubuntu-2004.eecis.udel.edu>
2023-11-26 12:34:16 -0500

    Signficant changes to kernel layout. This version ALMOST works! Still has floating point issues to fix.

--- pcompute.cu
+++ pcompute.cu
@@ -1,132 +1,154 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include "vector.h"
+#include "pvector.h"
 #include "config.h"
 
 #define THREAD_MAXIMUM 1024
 #define BLOCKS_PER_ROW (ceil((double)(NUMENTITIES) / (double)(THREAD_MAXIMUM)))
 #define THREADS_PER_BLOCK (THREAD_MAXIMUM < NUMENTITIES ? THREAD_MAXIMUM : NUMENTITIES)
+#define SPATIAL_AXES 3
 
-__global__ void calculateAccelerations (vector3** accels, vector3* hPos, double* masses) {
+
+__global__ void calcDists (vector3** dists, vector3* positions, double* masses) {
 
 	int row = blockIdx.x;
 	int col = (THREADS_PER_BLOCK * blockIdx.y) + threadIdx.x;
+	int axis = blockIdx.z;
 
 	if (NUMENTITIES <= col) return;
 
 	if (row == col) {
 
-		FILL_VECTOR(accels[row][col], 0, 0, 0);
+		dists[row][col][axis] = 0;
 
 	} else {
 
-		vector3 distance;
+		dists[row][col][axis] = positions[row][axis] - positions[col][axis];
 
-		for (int i = 0; i < 3; i++) {
-			distance[i] = hPos[row][i] - hPos[col][i];
-		}
+	}
+}
 
-		#ifdef STRICT_CALC_ACCELS
 
-		/**
-		 * Below's line of code is brought to you by CUDA's fused multiply-add. Fused multiply-add is faster and 
-		 * more accurate than separate operations, but it causes the math to differ from the CPU's math. 
-		 * So we need to do the below to disable it. Also it doesn't seem like fused multiple-add is *that* much
-		 * faster than doing it the regular way, so we should be fine keeping it like below.
-		 * 
-		 * Here is where I found the solution to this: 
-		 * https://stackoverflow.com/questions/14406364/different-results-for-cuda-addition-on-host-and-on-gpu
-		 * 
-		 * And here is some more in-depth reading: 
-		 * https://docs.nvidia.com/cuda/floating-point/index.html
-		 * 
-		 */
+__global__ void calcAccels (vector3** accels, vector3** dists, double* masses) {
 
-		double magnitude_sq = __dadd_rn(__dadd_rn(__dmul_rn(distance[0], distance[0]), __dmul_rn(distance[1], distance[1])), __dmul_rn(distance[2], distance[2]));
+	int row = blockIdx.x;
+	int col = (THREADS_PER_BLOCK * blockIdx.y) + threadIdx.x;
+	int axis = blockIdx.z;
+
+	if (NUMENTITIES <= col) return;
+
+	if (row == col) {
+
+		accels[row][col][axis] = 0;
+
+	} else {
+
+		#ifdef STRICT_ACCELS
+
+		double magnitude_sq = __dadd_rn(__dadd_rn(__dmul_rn(dists[row][col][0], dists[row][col][0]), __dmul_rn(dists[row][col][1], dists[row][col][1])), __dmul_rn(dists[row][col][2], dists[row][col][2]));
 
 		#else
 
-		double magnitude_sq = distance[0] * distance[0] + distance[1] * distance[1] + distance[2] * distance[2];
+		double magnitude_sq = dists[row][col][0] * dists[row][col][0] + dists[row][col][1] * dists[row][col][1] + dists[row][col][2] * dists[row][col][2];
 
 		#endif
 
 		double magnitude = sqrt(magnitude_sq);
 		double accelmag = -1 * GRAV_CONSTANT * masses[col] / magnitude_sq;
+		accels[row][col][axis] = accelmag * dists[row][col][axis] / magnitude;
 
-		FILL_VECTOR(
-			accels[row][col],
-			accelmag * distance[0] / magnitude,
-			accelmag * distance[1] / magnitude,
-			accelmag * distance[2] / magnitude
-		);
 	}
 }
 
-__global__ void sumAccelerations (vector3** accels, vector3* accel_sums) {
+__global__ void sumAccels (vector3** accels, vector3* accel_sums) {
 
 	int row = blockIdx.x;
 	int col = (THREADS_PER_BLOCK * blockIdx.y) + threadIdx.x;
+	int axis = blockIdx.z;
 
 	if (col != 0) return;
 
-	for (int i = 0; i < 3; i++) {
-		accel_sums[row][i] = 0;
-	}
+	accel_sums[row][axis] = 0;
 
 	for (int i = 0; i < NUMENTITIES; i++) {
-		for (int j = 0; j < 3; j++) {
-			accel_sums[row][j] += accels[row][i][j];
-		}
+
+		accel_sums[row][axis] += accels[row][i][axis];
+
 	}
+}
+
+__global__ void calcChanges (vector3* accel_sums, vector3* velocities, vector3* positions) {
+
+	int row = blockIdx.x;
+	int col = (THREADS_PER_BLOCK * blockIdx.y) + threadIdx.x;
+	int axis = blockIdx.z;
+
+	if (col != 0) return;
+
+	velocities[row][axis] += accel_sums[row][axis] * INTERVAL;
+	positions[row][axis] += velocities[row][axis] * INTERVAL;
 }
 
 void compute () {
 
-	dim3 blocks (NUMENTITIES, BLOCKS_PER_ROW);
+	dim3 blocks (NUMENTITIES, BLOCKS_PER_ROW, SPATIAL_AXES);
 	dim3 threads (THREADS_PER_BLOCK);
 
-	// Calculate Accelerations Starts Here
-
-	vector3* accel_values;
-	cudaMallocManaged(&accel_values, sizeof(vector3) * NUMENTITIES * NUMENTITIES);
-	vector3** accels;
-	cudaMallocManaged(&accels, sizeof(vector3*) * NUMENTITIES);
-
-	for (int i = 0; i < NUMENTITIES; i++) {
-		accels[i] = &accel_values[i * NUMENTITIES];
-	}
-
-	calculateAccelerations<<<blocks, threads>>>(accels, hPos, mass);
-	cudaError_t calculate_accelerations_error = cudaGetLastError();
-	if (calculate_accelerations_error != cudaSuccess) 
-		printf("calculateAccelerations kernel launch failed with Error: %s\n",
-			cudaGetErrorString(calculate_accelerations_error)
+	#ifdef DEBUG
+	cudaError_t e = cudaGetLastError();
+	if (e != cudaSuccess)
+		printf("Error before compute! %s: %s\n",
+			cudaGetErrorName(e),
+			cudaGetErrorString(e)
 		);
+	#endif
+
+	// Calculate Distances
+	calcDists<<<blocks, threads>>>(dists, device_positions, device_masses);
+	#ifdef DEBUG
+	cudaError_t calc_dists_error = cudaGetLastError();
+	if (calc_dists_error != cudaSuccess)
+		printf("calcDists kernel launch failed! %s: %s\n",
+			cudaGetErrorName(calc_dists_error),
+			cudaGetErrorString(calc_dists_error)
+		);
+	#endif
 	cudaDeviceSynchronize();
 
-	// Summing Accelerations Starts Here
-
-	vector3* accel_sums;
-	cudaMallocManaged(&accel_sums, sizeof(vector3) * NUMENTITIES);
-
-	sumAccelerations<<<blocks, threads>>>(accels, accel_sums);
-	cudaError_t sum_accelerations_error = cudaGetLastError();
-	if (sum_accelerations_error != cudaSuccess) 
-		printf("sumAccelerations kernel launch failed with Error: %s\n",
-			cudaGetErrorString(sum_accelerations_error)
+	// Calculate Accelerations
+	calcAccels<<<blocks, threads>>>(accels, dists, device_masses);
+	#ifdef DEBUG
+	cudaError_t calc_accels_error = cudaGetLastError();
+	if (calc_accels_error != cudaSuccess) 
+		printf("calcAccels kernel launch failed! %s: %s\n",
+			cudaGetErrorName(calc_accels_error),
+			cudaGetErrorString(calc_accels_error)
 		);
+	#endif
 	cudaDeviceSynchronize();
 
-	// Calculating Changes Starts Here
+	// Sum Accelerations
+	sumAccels<<<blocks, threads>>>(accels, accel_sums);
+	#ifdef DEBUG
+	cudaError_t sum_accels_error = cudaGetLastError();
+	if (sum_accels_error != cudaSuccess) 
+		printf("sumAccels kernel launch failed! %s: %s\n",
+			cudaGetErrorName(sum_accels_error),
+			cudaGetErrorString(sum_accels_error)
+		);
+	#endif
+	cudaDeviceSynchronize();
 
-	for (int i = 0; i < NUMENTITIES; i++) {
-		for (int j = 0; j < 3; j++){
-			hVel[i][j] += accel_sums[i][j] * INTERVAL;
-			hPos[i][j] += hVel[i][j] * INTERVAL;
-		}
-	}
-
-	cudaFree(accel_values);
-	cudaFree(accels);
+	// Calculating Changes
+	calcChanges<<<blocks, threads>>>(accel_sums, device_velocities, device_positions);
+	#ifdef DEBUG
+	cudaError_t calc_changes_error = cudaGetLastError();
+	if (calc_changes_error != cudaSuccess) 
+		printf("calcChanges kernel launch failed! %s: %s\n",
+			cudaGetErrorName(calc_changes_error),
+			cudaGetErrorString(calc_changes_error)
+		);
+	#endif
+	cudaDeviceSynchronize();
 }
