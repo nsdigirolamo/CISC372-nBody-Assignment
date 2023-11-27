847a1d4d4272275c8bf300b5168165a5927994d4
Nicholas DiGirolamo <nickdigi@ubuntu-2004.eecis.udel.edu>
2023-11-26 19:15:01 -0500

    Removed some comments, added some constants.

--- parallel/config.h
+++ parallel/config.h
@@ -7,15 +7,19 @@
 #define DAY 			HOUR*24
 #define WEEK 			DAY*7
 #define YEAR 			DAY*365
-//Configurable
+// Configurables
 #define NUMASTEROIDS 100
-#define GRAV_CONSTANT 6.67e-11 //the gravitational constant
+#define GRAV_CONSTANT 6.67e-11
 #define MAX_DISTANCE 5000.0
 #define MAX_VELOCITY 50000.0
-#define MAX_MASS 938e18  //approximate mass of ceres.
+#define MAX_MASS 938e18  // Approximate mass of Ceres
 #define DURATION (10*YEAR)
 #define INTERVAL DAY
-//End Configurable
+// My Configurables
+#define MAX_THREADS_PER_BLOCK 1024
+#define THREADS_PER_WARP 32
+#define MAX_WARPS_PER_BLOCK 32
+// End Configurables
 
 #define NUMENTITIES (NUMPLANETS+NUMASTEROIDS+1)
 #endif
