7d17a898199ba153937befb7986649fb3ce50a65
Nicholas DiGirolamo <nickdigi@ubuntu-2004.eecis.udel.edu>
2023-11-29 14:23:05 -0500

    Reorganized and added some kernel config args.

--- parallel/vector.h
+++ parallel/vector.h
@@ -4,22 +4,23 @@
 typedef double vector3[3];
 #define FILL_VECTOR(vector,a,b,c) {vector[0]=a;vector[1]=b;vector[2]=c;}
 
+// Host Memory
+
 extern vector3* host_velocities;
 extern vector3* host_positions;
 extern double* host_masses;
 
-extern int threads_per_row;
-extern int warps_per_row;
-extern int blocks_per_row;
-
-extern int threads_per_block;
-extern int warps_per_block;
+// Device Memory
 
 extern vector3* device_velocities;
 extern vector3* device_positions;
 extern double* device_masses;
 
 extern vector3** accels;
-extern vector3* accel_sums;
+
+// Kernel Config Arguments
+
+extern dim3 calc_changes_grid_dims;
+extern dim3 calc_changes_block_dims;
 
 #endif
