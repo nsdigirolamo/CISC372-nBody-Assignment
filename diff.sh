a4c7c4132a9a6a436c35490b3cfe71ac2e77249b
Nicholas DiGirolamo <nickdigi@ubuntu-2004.eecis.udel.edu>
2023-11-30 22:10:52 -0500

    Script now takes arguments.

--- diff.sh
+++ diff.sh
@@ -1,4 +1,17 @@
+if [ $# -e 0 ]
+
+then
+
 diff serial/out.txt parallel/out.txt \
     --old-group-format=$'\nOLD:\n\e[0;31m%<\e[0m' \
     --new-group-format=$'NEW:\n\e[0;31m%>\e[0m\n' \
     --unchanged-group-format=$'SAME:\n\e[0;32m%=\e[0m'
+
+else
+
+diff $1 $2 \
+    --old-group-format=$'\nOLD:\n\e[0;31m%<\e[0m' \
+    --new-group-format=$'NEW:\n\e[0;31m%>\e[0m\n' \
+    --unchanged-group-format=$'SAME:\n\e[0;32m%=\e[0m'
+
+fi
