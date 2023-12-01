52a325539e23fff05b68f38cbd401e5c1e683b4a
Nicholas DiGirolamo <nickdigi@ubuntu-2004.eecis.udel.edu>
2023-11-30 22:12:43 -0500

    Refined sum accels kernel. Now works greatgit add parallel/compute.cu

--- parallel/compute.cu
+++ parallel/compute.cu
@@ -4,17 +4,6 @@
 #include "vector.h"
 #include "config.h"
 
-int calcGridDim (int block_width, int entity_count) {
-
-	if (entity_count < block_width) return 1;
-
-	int grid_width = entity_count / block_width;
-
-	if (entity_count % block_width == 0) return grid_width;
-
-	return grid_width + 1;
-}
-
 __global__ void calcAccels (vector3** accels, vector3* positions, double* masses) {
 
 	int local_row = threadIdx.y;
@@ -56,50 +45,29 @@ __global__ void sumAccels (vector3** accels, int global_sum_length) {
 	int local_col = threadIdx.x;
 
 	int global_row = blockIdx.y;
-	// Double our offsets because each thread is assigned two data points globally
-	int global_col = (blockIdx.x * blockDim.x * 2) + (local_col * 2);
-	int spatial_axis = threadIdx.z;
+	int global_col = (blockIdx.x * blockDim.x) + local_col;
+	int spatial_axis = blockIdx.z;
 
-	__shared__ vector3 sums[SUM_LENGTH];
+	extern __shared__ double sums[];
+	int sums_length = blockDim.x;
 
-	/**
-	 * We need to zero out the values in sums because some local_cols may not be
-	 * needed. If they don't zero out their values, some other thread may try
-	 * to add a garbage value to their own good value.
-	 */
-	sums[local_col][spatial_axis] = 0;
+	sums[local_col] = 0;
 
 	if (global_sum_length <= global_col) return;
 
-	// Below initializes the shared array with summed global values.
+	sums[local_col] = accels[global_row][global_col][spatial_axis];
 
-	int offset = 1;
-	bool neighbor_exceeds_bounds = global_sum_length <= global_col + offset;
-
-	if (!neighbor_exceeds_bounds) {
-
-		sums[local_col][spatial_axis] = accels[global_row][global_col][spatial_axis] + accels[global_row][global_col + 1][spatial_axis];
-
-	} else {
-
-		sums[local_col][spatial_axis] = accels[global_row][global_col][spatial_axis];
-
-	}
-
-	// We will now treat the local_col as a thread id
-	int thread_id = local_col;
-
-	for (offset = 1; offset < SUM_LENGTH; offset *= 2) {
+	for (int offset = 1; offset < sums_length; offset *= 2) {
 		// This will produce a strided index from the thread_id
-		int i = 2 * offset * thread_id;
+		int i = 2 * offset * local_col;
 		// This allows for a non-divergent branch within the loop.
-		if (i < SUM_LENGTH) {
-			sums[i][spatial_axis] += sums[i + offset][spatial_axis];
+		if (i < sums_length) {
+			sums[i] += sums[i + offset];
 		}
 		__syncthreads();
 	}
 
-	if (local_col == 0) accels[global_row][blockIdx.x][spatial_axis] = sums[local_col][spatial_axis];
+	if (local_col == 0) accels[global_row][blockIdx.x][spatial_axis] = sums[local_col];
 }
 
 __global__ void calcChanges (vector3** accels, vector3* velocities, vector3* positions) {
@@ -152,14 +120,12 @@ void compute () {
 	// Sum Accelerations
 
 	int global_sum_length = NUMENTITIES;
-	dim3 sum_block_dims (SUM_LENGTH, 1, SPATIAL_DIMS);
 
-	while (1 < global_sum_length) {
+	while (1 < sum_accels_block_dims.x) {
 
-		int sum_grid_width = calcGridDim(SUM_LENGTH * 2, global_sum_length); // Multiply by two because each thread reduces two data points in accels.
-		dim3 sum_grid_dims (sum_grid_width, NUMENTITIES, 1);
+		setSumAccelsDims(global_sum_length, &sum_accels_grid_dims, &sum_accels_block_dims);
 
-		sumAccels<<<sum_grid_dims, sum_block_dims>>>(accels, global_sum_length);
+		sumAccels<<<sum_accels_grid_dims, sum_accels_block_dims, sizeof(double) * (sum_accels_block_dims.x)>>>(accels, global_sum_length);
 
 		#ifdef DEBUG
 		cudaError_t sum_accels_error = cudaGetLastError();
@@ -169,18 +135,18 @@ void compute () {
 				cudaGetErrorString(sum_accels_error)
 			);
 			printf("\tsumAccels Config: gridDims: {%d %d %d}, blockDims: {%d %d %d}\n",
-				sum_grid_dims.x,
-				sum_grid_dims.y,
-				sum_grid_dims.z,
-				sum_block_dims.x,
-				sum_block_dims.y,
-				sum_block_dims.z
+				sum_accels_grid_dims.x,
+				sum_accels_grid_dims.y,
+				sum_accels_grid_dims.z,
+				sum_accels_block_dims.x,
+				sum_accels_block_dims.y,
+				sum_accels_block_dims.z
 			);
 		}
 		fflush(stdout);
 		#endif
 
-		global_sum_length = sum_grid_width;
+		global_sum_length = sum_accels_grid_dims.x;
 	}
 
 	// Calculating Changes
