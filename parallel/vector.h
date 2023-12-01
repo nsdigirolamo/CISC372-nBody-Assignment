2e1ab9a19fc4219165c0b0dac108c85a7a5086c7
Nicholas DiGirolamo <nickdigi@ubuntu-2004.eecis.udel.edu>
2023-11-30 22:11:49 -0500

    Added new kernel args and function.

--- parallel/vector.h
+++ parallel/vector.h
@@ -26,4 +26,9 @@ extern dim3 calc_changes_block_dims;
 extern dim3 calc_accels_grid_dims;
 extern dim3 calc_accels_block_dims;
 
+extern dim3 sum_accels_grid_dims;
+extern dim3 sum_accels_block_dims;
+
+void setSumAccelsDims (int entity_count, dim3* grid_dims, dim3* block_dims);
+
 #endif
