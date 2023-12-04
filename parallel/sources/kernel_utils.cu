2b4b71df2f7f201e65ee7f64befec892e2042ece
Nicholas DiGirolamo <nickdigi@ubuntu-2004.eecis.udel.edu>
2023-12-04 16:45:55 -0500

    Fixed a lot of very embarrassing errors in setSumAccelsDims

--- parallel/sources/kernel_utils.cu
+++ parallel/sources/kernel_utils.cu
@@ -11,6 +11,18 @@ dim3 calc_accels_block_dims;
 dim3 sum_accels_grid_dims;
 dim3 sum_accels_block_dims;
 
+void printKernelDims (const char* identifier, dim3 grid_dims, dim3 block_dims) {
+	printf("%s: gridDims: {%d %d %d}, blockDims: {%d %d %d}\n",
+		identifier,
+		grid_dims.x,
+		grid_dims.y,
+		grid_dims.z,
+		block_dims.x,
+		block_dims.y,
+		block_dims.z
+	);
+}
+
 void initCalcAccelsDims () {
 
 	int grid_width = MINIMUM_DIM_SIZE;
@@ -60,47 +72,38 @@ void initCalcChangesDims () {
 	#endif
 }
 
-void setSumAccelsDims (int entity_count, dim3* grid_dims, dim3* block_dims) {
+void setSumAccelsDims (int entity_count) {
 
 	int grid_width = 1;
 
 	// Divide entity_count by 2 because each thread will handle two entities.
+	// Then, we add one if halved_entity_count is uneven to make sure we have
+	// enough threads. Then, we make sure halved_entity_count is a power of
+	// 2 to make sure our reduction algorithm works properly.
 
-	int round_up = entity_count % 2;
-	entity_count /= 2;
-	if (round_up) entity_count += 1;
+	int halved_entity_count = entity_count / 2;
+	if (entity_count % 2) halved_entity_count += 1;
+	halved_entity_count = pow(2, (int)ceil(log2(halved_entity_count)));
 
-	if (MAX_THREADS_PER_BLOCK < entity_count) {
-		grid_width = entity_count / MAX_THREADS_PER_BLOCK;
-		if (entity_count % MAX_THREADS_PER_BLOCK) grid_width += 1;
+	if (MAX_THREADS_PER_BLOCK < halved_entity_count) {
+		grid_width = halved_entity_count / MAX_THREADS_PER_BLOCK;
+		if (halved_entity_count % MAX_THREADS_PER_BLOCK) grid_width += 1;
 	}
 
-	int block_width = entity_count / grid_width;
-	if (entity_count % grid_width) block_width += 1;
+	int block_width = halved_entity_count / grid_width;
+	if (halved_entity_count % grid_width) block_width += 1;
 	int warp_offset = block_width % WARP_SIZE;
 	if (warp_offset) block_width += (WARP_SIZE - warp_offset);
 
-	grid_dims->x = grid_width;
-	grid_dims->y = entity_count;
-	grid_dims->z = SPATIAL_DIMS;
+	sum_accels_grid_dims.x = grid_width;
+	sum_accels_grid_dims.y = entity_count;
+	sum_accels_grid_dims.z = SPATIAL_DIMS;
 
-	block_dims->x = block_width;
-	block_dims->y = MINIMUM_DIM_SIZE;
-	block_dims->z = MINIMUM_DIM_SIZE;
+	sum_accels_block_dims.x = block_width;
+	sum_accels_block_dims.y = MINIMUM_DIM_SIZE;
+	sum_accels_block_dims.z = MINIMUM_DIM_SIZE;
 
 	#ifdef KERNEL_UTILS_DEBUG
-	printKernelDims("setSumAccelsDims", grid_dims, block_dims);
+	printKernelDims("setSumAccelsDims", sum_accels_grid_dims, sum_accels_block_dims);
 	#endif
-}
-
-void printKernelDims (const char* identifier, dim3 grid_dims, dim3 block_dims) {
-	printf("%s: gridDims: {%d %d %d}, blockDims: {%d %d %d}\n",
-		identifier,
-		grid_dims.x,
-		grid_dims.y,
-		grid_dims.z,
-		block_dims.x,
-		block_dims.y,
-		block_dims.z
-	);
 }
