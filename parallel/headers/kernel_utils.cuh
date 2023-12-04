67ef929a1ec8862e9b9915b6cb55bdcdb0bff15c
Nicholas DiGirolamo <nickdigi@ubuntu-2004.eecis.udel.edu>
2023-12-04 16:45:03 -0500

    Moved some function definitions around

--- parallel/headers/kernel_utils.cuh
+++ parallel/headers/kernel_utils.cuh
@@ -1,10 +1,10 @@
 #ifndef __KERNEL_UTILS_CUH__
 #define __KERNEL_UTILS_CUH__
 
+void printKernelDims (const char* identifier, dim3 grid_dims, dim3 block_dims);
 void initCalcAccelsDims ();
 void initCalcChangesDims ();
-void setSumAccelsDims (int entity_count, dim3* grid_dims, dim3* block_dims);
-void printKernelDims (const char* identifier, dim3 grid_dims, dim3 block_dims);
+void setSumAccelsDims (int entity_count);
 
 extern dim3 calc_changes_grid_dims;
 extern dim3 calc_changes_block_dims;
